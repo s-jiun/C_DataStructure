e4d120969755d04efb4dfb0ac40e98d1083102ec
Jiun <jiun48562@gmail.com>
2022-01-01 19:35:17 +0900

    [BFS]LinkedList_ver

--- 너비우선탐색.c
+++ 너비우선탐색.c
@@ -21,6 +21,12 @@ typedef struct GraphNode
 
 } GraphNode;
 
+typedef struct GraphType
+{
+    int n;
+    GraphNode *adj_list[MAX_VERTICES];
+} GraphType;
+
 int visited[MAX_VERTICES];
 
 void error(char *message)
@@ -58,4 +64,81 @@ element dequeue(QueueType *q)
         error("큐가 공백 상태입니다.");
     q->front = (q->front + 1) % MAX_QUEUE_SIZE;
     return q->queue[q->front];
+}
+
+void graph_init(GraphType *g)
+{
+    int v;
+    g->n = 0;
+    for (v = 0; v < MAX_VERTICES; v++)
+        g->adj_list[v] = NULL;
+}
+
+void insert_vertex(GraphType *g, int v)
+{
+    if (((g->n) + 1) > MAX_VERTICES)
+    {
+        fprintf(stderr, "그래프: 정점 개수 초과");
+        return;
+    }
+    g->n++;
+}
+
+void insert_edge(GraphType *g, int u, int v)
+{
+    GraphNode *node;
+    if (u >= g->n || v >= g->n)
+    {
+        fprintf(stderr, "그래프: 정점 번호 오류");
+        return;
+    }
+    node - (GraphNode *)malloc(sizeof(GraphNode));
+    node->vertex = v;
+    node->link = g->adj_list[u];
+    g->adj_list[u] = node;
+}
+
+void bfs_list(GraphType *g, int v)
+{
+    GraphNode *w;
+    QueueType *q;
+
+    queue_init(&q);
+    visited[v] = TRUE;
+    printf("%d 방문 -> ", v);
+    enqueue(&q, v);
+    while (!is_empty(&q))
+    {
+        v = dequeue(&q);
+        for (w = g->adj_list[v]; w; w = w->link)
+        {
+            if (!visited[w->vertex])
+            {
+                visited[w->vertex] = TRUE;
+                printf("%d 방문 -> ", w->vertex);
+                enqueue(&q, w->vertex);
+            }
+        }
+    }
+}
+
+int main()
+{
+    GraphType *g;
+    g = (GraphType *)malloc(sizeof(GraphType));
+    graph_init(g);
+    for (int i = 0; i < 6; i++)
+        insert_vertex(g, i);
+    insert_edge(g, 0, 2);
+    insert_edge(g, 2, 1);
+    insert_edge(g, 2, 3);
+    insert_edge(g, 0, 4);
+    insert_edge(g, 4, 5);
+    insert_edge(g, 1, 5);
+
+    printf("너비 우선 탐색\n");
+    bfs_list(g, 0);
+    printf("\n");
+    free(g);
+    return 0;
 }
